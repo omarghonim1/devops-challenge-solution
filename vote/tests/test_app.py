389dbd1663ebe8873d956e2160e166080fe99e6d
omarghonim1 <oghonim16@gmail.com>
2025-11-19 15:50:28 +0200

    Add initial test for pipeline testing

---
+++ vote/tests/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    response = client.get("/")
+    assert response.status_code in [200, 302]
