bdd14b8e8ce6dd0050a0246ec766047ccba480fe
omarghonim1 <oghonim16@gmail.com>
2025-11-21 01:37:57 +0200

    feat: hardening helm chart with persistence and secrets

---
+++ infra/outputs.tf
@@ -0,0 +1,14 @@
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "configure_kubectl" {
+  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
+}
